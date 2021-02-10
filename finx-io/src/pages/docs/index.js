196348ac66b86d4c08dacba50761518060041100
Geoff <geoff@fiteanalytics.com>
2021-02-10 08:48:28 -0800

    dev

--- finx-io/src/pages/docs/index.js
+++ finx-io/src/pages/docs/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Redirect } from '@reach/router'
+import { Redirect } from '@react/router'
 
 export default function Redirector({ path }) {
   return <Redirect from={path} to={`${path}v2/`} noThrow />
