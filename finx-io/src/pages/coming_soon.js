62ac90179df2ed4706281ec049d7c540bc04a139
Geoff <geoff@fiteanalytics.com>
2021-02-10 23:22:21 -0800

    geoff

--- finx-io/src/pages/coming_soon.js
+++ finx-io/src/pages/coming_soon.js
@@ -5,7 +5,7 @@ import BG from '../components/bg'
 const ComingSoonPage = props => (
   <Layout path={props.location.pathname}>
     <BG />
-    <SEO title="FINX is in Seed Token Raise. Learn more at the <a href='/token_sale'>Token Sale Page</a> path={props.location.pathname} />
+    <SEO title="FINX is in Seed Token Raise. Learn more at the <a href='/token_sale'>Token Sale Page</a>" path={props.location.pathname} />
     <div style={{maxWidth: 1440, padding: 32,margin: 'auto'}}>
       <h1>Coming Soon</h1>
       <p>You just found a page that doesn&#39;t exist yet but it will be very soon.</p>
