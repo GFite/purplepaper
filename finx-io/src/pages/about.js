e5a755932bcdfdaf745d88fd58be29783b90bd77
Geoff <geoff@fiteanalytics.com>
2021-02-12 14:31:03 -0800

    geoff

--- finx-io/src/pages/about.js
+++ finx-io/src/pages/about.js
@@ -271,10 +271,13 @@ const About = props => {
               FINX is a Decentralized Exchange (<span><strong>DEX</strong></span>) that provides a way for all Capital Market Participants
               to conduct decentralized commerce in crypto.
             </p>
-            <p>We are committed to building more efficient counterparty arrangements on the decentralized web.</p>
-
+            <p>Underwriters, Insurers, Ratings Agencies, Collateral Valuators and others can provide services to Corporate
+            Borrowers that act as collateral against Collateralized Pools on the Ethereum Mainnet. The ERC20 contracts stipulate
+            repayment periods, interest models, liquidity provisions, workout and dispute resolution.</p>
+            <p>FINX committed to building more efficient counterparty arrangements on the decentralized web.</p>
+            <h3>FINX is currently conducting a [<strong>Genesis Token Sale</strong>](/token_sale/) from February 15 through March 15.</h3>
             <div style={{ display: 'flex', width: '100%', margin: 0 }}>
-              <InternalLink to="/blog/uni">FINX token</InternalLink>
+              <InternalLink to="/blog/uni"><img src="../../images/fav.png"/>FINX token</InternalLink>
               <InternalLink to="./whitepaper.pdf">
                 Whitepaper <span style={{ fontSize: '11px' }}>↗</span>
               </InternalLink>
