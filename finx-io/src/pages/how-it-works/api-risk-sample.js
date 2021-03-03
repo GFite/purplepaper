a71b716763402f5f51582586d784d1c5fc5cb19d
GFite <geoff@fiteanalytics.com>
2021-03-02 18:54:31 -0800

    geoff

--- finx-io/src/pages/how-it-works/api-risk-sample.js
+++ finx-io/src/pages/how-it-works/api-risk-sample.js
@@ -1,8 +1,1 @@
----
-title: API Cash Flow Example
-subtitle: How to Use the Cash Flow API
-tags: api, documentation
----
-
-This document is a tutorial on using the Cash Flow API.
 
--- finx-io/src/pages/how-it-works/sample-portfolio-report.js
+++
@@ -1,8 +0,0 @@
----
-title: Sample Portfolio Report
-subtitle: How to Use the Cash Flow API
-tags: api, documentation
----
-
-This document is an example of a Portfolio Report.
-
